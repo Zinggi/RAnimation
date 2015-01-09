cb0c46b204fddb529e8805760508c2e581701cb8
Florian Zinggeler <florian.zinggeler@gmail.com>
2015-01-09 16:24:33 +0100

    performance.now polyfill

--- src/animation/polyfills.jsx
+++ src/animation/polyfills.jsx
@@ -9,3 +9,20 @@ window.requestAnimationFrame = (function(){
         window.setTimeout(callback, 1000 / 60);
       };
 })();
+
+
+(function(){
+	// prepare base perf object
+	if (typeof window.performance === 'undefined') {
+		window.performance = {};
+	}
+	if (!window.performance.now){
+		var nowOffset = Date.now();
+		if (performance.timing && performance.timing.navigationStart){
+			nowOffset = performance.timing.navigationStart;
+		}
+		window.performance.now = function now(){
+			return Date.now() - nowOffset;
+		};
+	}
+})();
